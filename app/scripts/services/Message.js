a7a43388e1f5fb40a27733f1b0f5797dfd100848
Quentin Guillaume <quent.guillaume@gmail.com>
2017-05-07 17:40:15 -0700

    bloc 4 done

--- app/scripts/services/Message.js
+++ app/scripts/services/Message.js
@@ -1,17 +1,17 @@
 (function() {
-  function Message($firebaseArray) {
-    return {
-        getByRoomId: function(id) {
-          // Filter the messages by their room ID.
-          var messages = $firebaseArray(firebase.database().ref().child("messages").orderByChild("roomId").equalTo(id));
-          return messages;
-        }
-      };
-  }
+	function Message($firebaseArray) {
+		var ref = firebase.database().ref().child("messages");
+		var messages = $firebaseArray(ref);
 
+		return {
+			getByRoomId: function (roomId) {
+				// Filter the messages by their room ID.
+				return $firebaseArray(ref.orderByChild('roomId').equalTo(roomId));
+			}
+		};
+	}
 
-
-  angular
-    .module('blocChat')
-    .factory('Message', ['$firebaseArray', Message]);
+	angular
+		.module('blocChat')
+		.factory('Message', ['$firebaseArray', Message]);
 })();
